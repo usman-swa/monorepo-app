26acfcaa028239909fcb6e9218e98a151afee51b
usman.amadeus <usman.muhammad@amadeus.com>
2020-06-10 21:40:49 +0400

    Add front end

---
+++ packages/front/src/App.tsx
@@ -0,0 +1,22 @@
+import React, { useState, useEffect } from "react";
+
+function App() {
+  const [foo, setFoo] = useState("N/A");
+  useEffect(() => {
+    fetch("/api/v1/examples")
+      .then((res) => res.json())
+      .then((data) => setFoo(data.foo))
+      .catch((err) => setFoo(err.message));
+  });
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <p>
+        Server responded with foo:
+        {foo}
+      </p>
+    </div>
+  );
+}
+
+export default App;
